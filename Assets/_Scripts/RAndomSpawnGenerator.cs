8c26b3554b9b97c46e41ebe38d1f49e4f093c94c
MyonggwanSong <spooh01@naver.com>
2025-01-21 14:05:48 +0900

    v0.0.3 feat: 프랍배치 툴 만들기
    반복문, 랜덤, 범위설정으로 프랍배치 툴 만들기

--- Assets/_Scripts/RAndomSpawnGenerator.cs
+++ Assets/_Scripts/RAndomSpawnGenerator.cs
@@ -1,71 +1,118 @@
+using System.Collections.Generic;
 using CustomInspector;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class RAndomSpawnGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject prefab;
+    [SerializeField] [Preview]List<GameObject> prefabs;
     [SerializeField] Transform propRoot;
 
 
-    void OnDrawGizmos() // 에디터에서 기즈모를 그릴 수 있는 공간
+    void OnDrawGizmos()      // 에디터에서 기즈모를 그릴 수 있는 공간
     {
-        Vector3 temp = new Vector3(transform.position.x, y, transform.position.z);
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(temp, Radius);
+        if (deleteOn == false)
+        {
+            Vector3 temp = new Vector3(transform.position.x, hieght, transform.position.z);
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(temp, Radius);
+        }
 
-        Gizmos.DrawWireCube(temp,Vector3.one*Radius);
+        // Gizmos.DrawWireCube(temp,Vector3.one*Radius);
+        else
+        {
+            Vector3 destroied = new Vector3(transform.position.x, hieght, transform.position.z);
+            Gizmos.color = Color.gray;
+            Gizmos.DrawWireSphere(destroied, Radius);
+        }   
 
+        // Gizmos.DrawWireCube(temp,Vector3.one*Radius);
     }
-
 
     [Space(20), HorizontalLine("스포너", color: FixedColor.Orange), HideField] public bool _l1;
-    [Button("Spawn",size = Size.big), HideField] public bool _b0;
-    [SerializeField] float Radius;
-    [SerializeField] float y;
-    [SerializeField] int number;
-    [SerializeField] [AsRange(0.5f,2f)] Vector2 scaleRange;
+    [Button("Spawn", size = Size.big), HideField] public bool _b0;
+    [Title("범위 속성", fontSize = 15, alignment = TextAlignment.Center), HideField] public bool _t0;
+    [SerializeField,Space(10)] float Radius;
+    [SerializeField] float hieght;
+    [Space(20), HideField] public bool _s0;
+    [Title("객체 속성", fontSize = 15, alignment = TextAlignment.Center), HideField] public bool _t1;
+    [SerializeField,Space(10), AsRange(1, 1000)] Vector2 number;
+    [SerializeField][AsRange(0.0f, 2f)] Vector2 scaleRange;
     float angle;
+    public void add()
+    {
+        // Instantiate(prefabs, new Vector3(x, y, z), Quaternion.identity);
+        
+            int rndindex = Random.Range(0,prefabs.Count);
+            float scale = Random.Range(scaleRange.x, scaleRange.y);                           // 스케일 랜덤 돌리기
+            angle = Random.Range(0f, 360.0f);                                                 // 랜덤 로테이션 찍기    
+            GameObject clone = Instantiate(prefabs[rndindex]); /*출력*/                       // 클론 찍기(출력)
+
+            Vector3 rndpos = Random.insideUnitSphere * Radius + transform.position;           // 포지션 반지름 곱한 x,z만 랜덤 돌리고 
+
+            Vector3 temp = new Vector3(transform.position.x, hieght, transform.position.z);   // temp에 y빼고 클론의 포지션 지정
+            rndpos.y = temp.y;                                                                // temp y값을 랜덤포지션에 넣기
+            Vector3 temp2 = new Vector3(scale, scale, scale);                                 // temp2에 랜덤된 scale 입력
+            Vector3 temp3 = new Vector3(0.0f, angle, 0.0f);                                   // temp3에 램덤된 y회전율 넣기
+
+
+            // add = Random.insideUnitSphere
+            clone.transform.position = rndpos;                                                 // 클론 포지션은 랜덤포지션
+            clone.transform.localScale = temp2;                                                // 클론 스케일은 temp2
+            clone.transform.localEulerAngles = temp3;                                          // 클론 회전은 temp3
+            clone.transform.SetParent(propRoot);                                               // 클론 부모는 propRoot 안에
+        
+
+    }
     public void Spawn()
     {
-        // Instantiate(prefab, new Vector3(x, y, z), Quaternion.identity);
-
-        for (int a = 0; a < number; a++)
+        if (deleteOn == false)
         {
-        float scale = Random.Range(scaleRange.x,scaleRange.y);                                            // 스케일 랜덤 돌리기
-        angle = Random.Range(0f,360.0f);
-        GameObject clone = Instantiate(prefab);                                     // 클론 찍기(출력)
-        Vector3 rndpos = Random.insideUnitSphere * Radius + transform.position;     // 포지션 반지름 곱한 x,z만 랜덤 돌리고 
-
-        Vector3 temp = new Vector3(transform.position.x, y, transform.position.z);  // temp에 y빼고 클론의 포지션 지정
-        Vector3 temp2 = new Vector3(scale, scale, scale);                           // temp2에 랜덤된 scale 입력
-        Vector3 temp3 = new Vector3(0.0f, angle, 0.0f);
-        rndpos.y = temp.y;                                                          // temp에 y값 넣기
-
-
-        // add = Random.insideUnitSphere
-            clone.transform.position = rndpos;
-            clone.transform.localScale = temp2;
-            clone.transform.localEulerAngles = temp3;
-            clone.transform.SetParent(propRoot);
+            int i = (int)Random.RandomRange(number.x, number.y);
+            for (int a = 0; a < i; a++)
+            {
+                add();
+            }
         }
+        else
+        Debug.Log("!!삭제!!의 \"Delete on\"을 꺼주세요");
 
     }
 
+    [Space(20), HorizontalLine("!!삭제!!", color: FixedColor.Orange), HideField] public bool _l2;
+    public bool deleteOn;
 
-    [Space(20), Button("Despawn", size = Size.big), HideField] public bool _b1;
-    void Despawn()
+
+    bool CheckInside(Vector3 A, Vector3 B)                                                    // 두점 사이의 거리를 구하는 함수
+    {
+        float dist = Vector3.Distance(A, B);
+        return dist <= Radius;
+    }
+
+    [Space(20),Button("Erase", size = Size.big), HideField] public bool _b1;
+    void Erase()
 
     {
-        // propRoot.transform.childCount : 리스트 자료구조형
-        // Destroy() : 플레이 모드일 때        
-        
-        foreach (Transform t in propRoot)
+        if (deleteOn == true)
         {
-           DestroyImmediate(t.gameObject); /*에디터 모드일 때*/
-        
+            // propRoot.transform.childCount : 리스트 자료구조형
+
+            // foreach (Transform t in propRoot)
+            // {
+            //     DestroyImmediate(t.gameObject); /*에디터 모드일 때*/  // Destroy() : 플레이 모드일 때 
+            // }
+
+            foreach (Transform t in propRoot)
+            {
+                if (CheckInside(t.position, transform.position) == true)
+                    DestroyImmediate(t.gameObject);
+            }
         }
+
+
+        else
+        Debug.Log("\"Delete on\"을 켜주세요");
     }
+
 
 
 }
